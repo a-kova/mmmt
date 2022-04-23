e8940f37fc3663382ee4534acf6a4a4a56e1e5b5
Kovalchuk91 <Kovalchuk91@users.noreply.github.com>
2022-04-23 11:00:22 +0000

    deploy: c4efffedd2a65fc551c1206d32e543e7807403c0

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,32 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const body = document.body;
-    const themeSwitcher = document.getElementById('theme-switcher');
+const switchToLight = (body) => {
+  body.dataset.theme = "light";
+  localStorage.setItem("theme", "light");
+};
 
-    themeSwitcher && themeSwitcher.addEventListener('click', () => {
-        if (body.dataset.theme == 'dark') {
-            body.dataset.theme = 'light';
-        } else {
-            body.dataset.theme = 'dark';
-        }
+const switchToDark = (body) => {
+  body.dataset.theme = "dark";
+  localStorage.setItem("theme", "dark");
+};
+
+let prefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+let initialTheme = localStorage.getItem("theme");
+if (!initialTheme) {
+  initialTheme = prefersDark ? "dark" : "light";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.body;
+  const themeSwitcher = document.getElementById("theme-switcher");
+
+  body.dataset.theme = initialTheme;
+
+  setTimeout(() => {
+    body.classList.add("loaded");
+    themeSwitcher.classList.add("loaded");
+  }, 0);
+
+  themeSwitcher && themeSwitcher.addEventListener("click", () => {
+      body.dataset.theme == "dark" ? switchToLight(body) : switchToDark(body);
     });
 });
